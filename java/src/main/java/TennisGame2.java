ae16533a9aff5a723627deedfb8aa053aa9b3092
DanDs <dany.gaara.ds@gmail.com>
2017-02-24 04:43:56 -0400

    eliminacion de variables no utilizadas2

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -5,13 +5,9 @@ public class TennisGame2 implements TennisGame
     public int P2point = 0;
     
     public String P1res = "";
-    public String P2res = "";
-    private String player1Name;
-    private String player2Name;
+    public String P2res = "";    
 
-    public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame2(String player1Name, String player2Name) {        
     }
 
     public String getScore(){
